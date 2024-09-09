a48a4b800581e4a1b2a39c545e1b98da7a8f2207
Archalos0 <tngblondin.prog@gmail.com>
2024-09-09 11:19:21 +0200

    delete old commented code

--- scripts/battle_menu.gd
+++ scripts/battle_menu.gd
@@ -5,9 +5,6 @@ class_name BattleMenu extends Control
 @onready var escape_button: ActionButton = $ActionSelection/VBoxContainer/EscapeButton
 @onready var heal_button: ActionButton = $ActionSelection/VBoxContainer/HealButton
 
-#@onready var stats: StatsUI = $Informations/CharacterData/Stats
-#@onready var character_name: Label = $Informations/CharacterData/CharacterName
-
 @onready var action_selection: MenuHandler = $ActionSelection
 @onready var target_selection: MenuHandler = $TargetSelection
 @onready var player_character_informations: PlayerSelectionMenu = $PlayerCharacterInformations
@@ -37,18 +34,3 @@ func set_focus_on_player_character():
 	player_character_informations.get_focus()
 	target_selection.lost_focus()
 	action_selection.lost_focus()
-
-#func set_character_name(character):
-	#character_name.text = character.character_name
-
-#func set_stats(character: Character):
-	#stats.set_stat(stats.STAT.HP, character.stats.health)
-	#stats.set_stat(stats.STAT.MP, character.stats.magic_point)
-	#stats.set_stat(stats.STAT.STR, character.stats.strenght)
-	#stats.set_stat(stats.STAT.MAG, character.stats.magic)
-	#stats.set_stat(stats.STAT.DEF, character.stats.defense)
-	#stats.set_stat(stats.STAT.MDEF, character.stats.magic_defense)
-	#stats.set_stat(stats.STAT.AGI, character.stats.agility)
-	#stats.set_stat(stats.STAT.ACC, character.stats.accuracy)
-	#stats.set_stat(stats.STAT.EVA, character.stats.evasion)
-	#stats.set_stat(stats.STAT.LUC, character.stats.luck)
