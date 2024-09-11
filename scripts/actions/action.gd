f7ab98f81dec2b8dea09498ec29c5741504d6220
Archalos0 <tngblondin.prog@gmail.com>
2024-09-11 11:16:08 +0200

    add a damage type to the action

--- scripts/actions/action.gd
+++ scripts/actions/action.gd
@@ -15,12 +15,6 @@ enum ACTION_CLASS {
 	BLACK_MAGIC,
 }
 
-enum ACTION_TYPE {
-	BASIC_ATTACK,
-	PHYSICAL_ATTACK,
-	MAGICAL_ATTACK,
-}
-
 enum ELEMENT {
 	FIRE,
 	ICE,
@@ -55,20 +49,21 @@ enum DEBUFF {
 	DEFENSE_NONE
 }
 
-class Spell:
-	var element: ELEMENT
-	var power: int
-	var buffs: Array[BUFF]
-	var debuffs: Array[DEBUFF]
-	var is_offensive: bool
+enum DAMAGE_TYPE {
+	NONE,
+	PHYSICAL,
+	MAGICAL,
+	HEALING,
+}
 
 @export var action_name: String = ""
 @export var description: String = "Description"
 @export var cost: int = 0
 @export var target_type: TARGET_TYPE = TARGET_TYPE.SINGLE_ENEMY
-@export var action_type: ACTION_TYPE
+#@export var action_type: ACTION_TYPE
 
 @export var spell_power: int
+@export var damage_type: DAMAGE_TYPE
 @export var buffs: Array[BUFF]
 @export var debuffs: Array[DEBUFF]
 @export var element: ELEMENT
@@ -78,19 +73,21 @@ func _init(
 	p_description: String, 
 	p_cost: int, 
 	p_target_type: TARGET_TYPE, 
-	p_action_type: ACTION_TYPE, 
+	#p_action_type: ACTION_TYPE, 
 	p_spell_power: int, p_buffs: Array[Action.BUFF], 
 	p_debuffs: Array[Action.DEBUFF], 
-	p_element: ELEMENT):
+	p_element: ELEMENT,
+	p_damage_type: DAMAGE_TYPE):
 		action_name = p_name
 		description = p_description
 		cost = p_cost
 		target_type = p_target_type
-		action_type = p_action_type
+		#action_type = p_action_type
 		spell_power = p_spell_power
 		buffs = p_buffs
 		debuffs = p_debuffs
 		element = p_element
+		damage_type = p_damage_type
 
 static func from_action_resource(action_resource: ActionResource) -> Action:
 	var action: Action = Action.new(
@@ -98,11 +95,12 @@ static func from_action_resource(action_resource: ActionResource) -> Action:
 		action_resource.description,
 		action_resource.cost,
 		action_resource.target_type,
-		action_resource.action_type,
+		#action_resource.action_type,
 		action_resource.spell_power,
 		action_resource.buffs,
 		action_resource.debuffs,
-		action_resource.element
+		action_resource.element,
+		action_resource.damage_type
 	)
 	
 	return action
@@ -117,26 +115,31 @@ func is_targeting_allies() -> bool:
 	return true if target_type == TARGET_TYPE.SINGLE_ALLY or target_type == TARGET_TYPE.ALL_ALLIES else false
 
 func calcul_power(source: Character):
-	var spell_power = 0
+	var power = 0
 	
-	match action_type:
-		ACTION_TYPE.BASIC_ATTACK:
-			spell_power = (source.stats.strenght / 4) + 8 #+8 = temp # need to add after : + (source.skill / 4) + source.weapon.damage
-		ACTION_TYPE.PHYSICAL_ATTACK:
-			spell_power = source.stats.strenght * spell_power
-		ACTION_TYPE.MAGICAL_ATTACK:
-			spell_power = source.stats.intellect * spell_power
+	match damage_type:
+		DAMAGE_TYPE.PHYSICAL:
+			power = (source.stats.strenght / 4) + 8
+		DAMAGE_TYPE.MAGICAL:
+			power = source.stats.intellect * spell_power / 4
+		DAMAGE_TYPE.HEALING:
+			power = source.stats.mind * spell_power / 4
+		_:
+			power = 0
 	
-	return spell_power
+	return power
 
 #TODO: Add the element, the weapon, and additional effects
 func execute(source: Character, targets: Array[Character]):
-	var spell: Spell = Spell.new()
-	spell.power = calcul_power(source)
-	spell.buffs = buffs
-	spell.debuffs = debuffs
-	spell.element = element
-	spell.is_offensive = true if is_targeting_enemies() else false
+	var power = calcul_power(source)
 	
-	for target: Character in targets:
-		target.receive_spell(spell)
+	match damage_type:
+		DAMAGE_TYPE.PHYSICAL:
+			for target: Character in targets:
+				target.get_hit(power - target.stats.defense)
+		DAMAGE_TYPE.MAGICAL:
+			for target: Character in targets:
+				target.get_hit(power - target.stats.magic_defense)
+		DAMAGE_TYPE.HEALING:
+			for target: Character in targets:
+				target.get_heal(power)
