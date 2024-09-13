da964b85c359b772885efaf9c1cd8b920fe35cd8
Archalos0 <tngblondin.prog@gmail.com>
2024-09-13 12:17:29 +0200

    fix: variable name

--- scripts/characters/stats.gd
+++ scripts/characters/stats.gd
@@ -3,7 +3,7 @@ class_name Stats extends Node
 @export var health_point: int
 @export var magic_point: int
 
-@export var strenght: int
+@export var strength: int
 @export var agility: int
 @export var vitality: int
 @export var intellect: int
@@ -12,4 +12,21 @@ class_name Stats extends Node
 @export var magic_defense: int
 
 func _init():
-	pass
+	strength 		= 5
+	agility 		= 5
+	vitality 		= 5
+	intellect 		= 5
+	mind 			= 5
+	defense 		= 5
+	magic_defense 	= 5
+
+static func from_dictionary(data: Dictionary) -> Stats:
+	var new_stats = Stats.new()
+	
+	new_stats.strength 	= data.strength
+	new_stats.agility 	= data.agility
+	new_stats.vitality	= data.vitality
+	new_stats.intellect	= data.intellect
+	new_stats.mind		= data.mind
+	
+	return new_stats
