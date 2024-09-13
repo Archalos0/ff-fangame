29147c31a94918f9a776b6aee2080b2326526931
Archalos0 <tngblondin.prog@gmail.com>
2024-09-13 16:19:59 +0200

    add a player character and update scripts

---
+++ ressources/characters/player_character_resource.gd
@@ -0,0 +1,7 @@
+class_name PlayerCharacterResource extends Resource
+
+@export var character_name: String
+@export var job_resource: JobResource
+@export var level: int = 1
+
+#equipments
--- scripts/job/job.gd
+++ scripts/job/job.gd
@@ -15,18 +15,28 @@ var actions: Array[Action] = []
 # Equippable weapons
 # var equippable_weapons = Array[WEAPON_TYPE]
 
-const JOBS_STATS = preload("res://scripts/job/jobs_stats.json")
+@export var stats_evolution: Dictionary
 
-@export var jobs_stats: Dictionary = {}
+#func _init(p_job_name: String, p_level: int):
+	#job_name = p_job_name
+	#level = p_level
+	#
+	#jobs_stats = JOBS_STATS.data
+	#var current_job_stats: Array = jobs_stats[job_name]
+	#for i in range(current_job_stats.size()):
+		#if current_job_stats[i].level == level:
+			#stats = Stats.from_dictionary(current_job_stats[i].stats)
+			#break
+			#
 
-func _init(p_job_name: String, p_level: int):
-	job_name = p_job_name
-	level = p_level
+static func from_resource(job_resource: JobResource, character_level: int):
+	var new_job = Job.new()
+	new_job.job_name = job_resource.job_name
 	
-	jobs_stats = JOBS_STATS.data
-	var current_job_stats: Array = jobs_stats[job_name]
-	for i in range(current_job_stats.size()):
-		if current_job_stats[i].level == level:
-			stats = Stats.from_dictionary(current_job_stats[i].stats)
-			break
-			
+	for action_resource: ActionResource in job_resource.actions_resources:
+		new_job.actions.append(Action.from_action_resource(action_resource))
+		
+	new_job.stats_evolution = job_resource.stats_evolution.data
+	new_job.stats = Stats.from_dictionary(new_job.stats_evolution[str(character_level)])
+	
+	return new_job
