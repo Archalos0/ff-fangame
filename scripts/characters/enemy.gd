d10447abf730b3fe5eaa928a8f96425ca1745374
Archalos0 <tngblondin.prog@gmail.com>
2024-09-16 12:08:31 +0200

    rename abstract function

--- scripts/characters/enemy.gd
+++ scripts/characters/enemy.gd
@@ -3,6 +3,9 @@ class_name Enemy extends Character
 var gils: int
 var experience: int
 
+func load_from_character_resource(p_resource: CharacterResource):
+	push_error("Need to implement from_character_resource() for enemy")
+
 func load_actions():
 	push_warning("Need to implement load_actions() for enemy")
 	pass
