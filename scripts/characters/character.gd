2f81fb6823891bb5b742c9630376e083cc1e18f6
Archalos0 <tngblondin.prog@gmail.com>
2024-09-16 12:07:57 +0200

    update Character abstract class

--- scripts/characters/character.gd
+++ scripts/characters/character.gd
@@ -1,11 +1,16 @@
 class_name Character
 
 var character_name: String
-var sprite: Sprite2D
+var texture: Texture2D
 var level: int
 
 var stats: Stats
 var actions: Array[Action]
+
+var character_resource: CharacterResource
+
+func load_from_character_resource(p_resource: CharacterResource):
+	push_error("The method from_character_resource() has not been implementing in the derived class")
 
 func load_actions():
 	push_error("The method load_actions() has not been implementing in the derived class")
