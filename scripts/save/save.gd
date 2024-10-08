5b8e2faddec2e4aa9558e0320328e61f8aa1eaf6
Archalos0 <tngblondin.prog@gmail.com>
2024-10-08 11:38:18 +0200

    save data into json

--- scripts/save/save.gd
+++ scripts/save/save.gd
@@ -12,7 +12,7 @@ class CharacterData:
 				"job": job = dict["job"]
 	
 	func _to_string() -> String:
-		return "| " + name + " | " + job + " |"
+		return "qqqqq| " + name + " | " + job + " |"
 #---------------#
 
 const KEYS := ["characters"]
@@ -27,6 +27,7 @@ func _init(file_path: String) -> void:
 	
 	var file = FileAccess.open(file_path, FileAccess.READ)
 	var res = json.parse(file.get_as_text())
+	file.close()
 	
 	if res == OK:
 		var raw_data = json.data
@@ -43,3 +44,37 @@ func _init(file_path: String) -> void:
 func _load_characters(data: Array) -> void:
 	for dict in data:
 		characters.append(CharacterData.new(dict))
+
+
+static func save():
+	var new_save: Save
+	
+	var data: Dictionary = {
+		1: { 
+			"name": "Luneth",
+			"job": "Thief" 
+		},
+		2: {
+			"name": "Arc",
+			"job": "Black Mage" 
+		},
+		3: {
+			"name": "Refia",
+			"job": "White Mage" 
+		}
+	}
+	
+	var new_dict: Dictionary = {
+		"characters": []
+	}
+	for i in range(data.size()):
+		new_dict["characters"].append(data[i+1])
+	
+	var json_string := JSON.stringify(new_dict, "\t")
+	
+	var file := FileAccess.open("res://scripts/save/save.json", FileAccess.WRITE)
+	
+	file.store_string(json_string)
+	file.close()
+	
+	
--- scripts/save/test.gd
+++ scripts/save/test.gd
@@ -2,4 +2,4 @@ extends Node2D
 
 
 func _ready() -> void:
-	Save.new("res://scripts/save/save.json")
+	Save.save()
