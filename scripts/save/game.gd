14a6883127ed4324f4770ed6a1b62686bdc43bf5
Archalos0 <tngblondin.prog@gmail.com>
2024-10-09 16:55:20 +0200

    load only id of the equipment

--- scripts/save/game.gd
+++ scripts/save/game.gd
@@ -43,73 +43,69 @@ class CharacterData:
 			"name": name,
 			"job": job,
 			"equipments": {
-				"left_hand": equipments.left_hand.id,
-				"right_hand": equipments.right_hand.id,
-				"head_armor": equipments.head_armor.id,
-				"body_armor": equipments.body_armor.id,
-				"arm_armor": equipments.arm_armor.id,
+				"left_hand": equipments.left_hand,
+				"right_hand": equipments.right_hand,
+				"head_armor": equipments.head_armor,
+				"body_armor": equipments.body_armor,
+				"arm_armor": equipments.arm_armor,
 			}
 		}
 		
 		return properties
 
 class EquipmentsData:
-	var left_hand: EquipmentData = EquipmentData.new()
-	var right_hand: EquipmentData = EquipmentData.new()
-	var head_armor: EquipmentData = EquipmentData.new()
-	var body_armor: EquipmentData = EquipmentData.new()
-	var arm_armor: EquipmentData = EquipmentData.new()
+	var left_hand:  String = ""
+	var right_hand: String = ""
+	var head_armor: String = ""
+	var body_armor: String = ""
+	var arm_armor:  String = ""
 	
 	func load_equipments(dict: Dictionary):
 		for key in dict:
-			if dict[key] == null or dict[key] == "":
+			if dict[key] == null:
 				continue
 			
 			match key:
-				"left_hand": 
-					left_hand.load_equipment(dict["left_hand"])
-				"right_hand": 
-					right_hand.load_equipment(dict["right_hand"])
-				"head_armor": 
-					head_armor.load_equipment(dict["head_armor"])
-				"body_armor": 
-					body_armor.load_equipment(dict["body_armor"])
-				"arm_armor":
-					arm_armor.load_equipment(dict["arm_armor"])
+				"left_hand": left_hand = dict["left_hand"]
+				"right_hand": right_hand = dict["right_hand"]
+				"head_armor": head_armor = dict["head_armor"]
+				"body_armor": body_armor = dict["body_armor"]
+				"arm_armor": arm_armor = dict["arm_armor"]
 
-class EquipmentData:
-	var name: String
-	var id: String
-	var type: String
-	
-	var attack: int
-	var defense: int
-	var magic_defense: int
-	var evade: int
-	
-	var sprite_path: String
-	var stats_upgraded: Array
-	
-	func load_equipment(equipment_id: String):
-		var dict: Dictionary = FileHandler.get_json_content("res://ressources/equipments/equipments.json")
-		
-		if dict.has("error"):
-			return
-		
-		var equipment_data: Dictionary = dict[equipment_id]
-		
-		id = equipment_id
-		
-		name = equipment_data["name"]
-		type = equipment_data["type"]
-		
-		attack = equipment_data.get("attack", 0)
-		defense = equipment_data.get("defense", 0)
-		magic_defense = equipment_data.get("magic_defense", 0)
-		evade = equipment_data.get("evade", 0)
-		
-		sprite_path = equipment_data["sprite_path"]
-		stats_upgraded = equipment_data.get("stats_upgraded", null)
+
+#class EquipmentData:
+	#var name: String
+	#var id: String
+	#var type: String
+	#
+	#var attack: int
+	#var defense: int
+	#var magic_defense: int
+	#var evade: int
+	#
+	#var sprite_path: String
+	#var stats_upgraded: Array
+	#
+	#func load_equipment(equipment_id: String):
+		#var dict: Dictionary = FileHandler.get_json_content("res://ressources/equipments/equipments.json")
+		#
+		#if dict.has("error"):
+			#return
+		#
+		#var equipment_data: Dictionary = dict[equipment_id]
+		#
+		#id = equipment_id
+		#
+		#name = equipment_data["name"]
+		#type = equipment_data["type"]
+		#
+		#attack = equipment_data.get("attack", 0)
+		#defense = equipment_data.get("defense", 0)
+		#magic_defense = equipment_data.get("magic_defense", 0)
+		#evade = equipment_data.get("evade", 0)
+		#
+		#sprite_path = equipment_data["sprite_path"]
+		#stats_upgraded = equipment_data.get("stats_upgraded", null)
 #-------------------#
 
 var _save_file_path: String = ""
@@ -149,5 +145,5 @@ func add_character(dict: Dictionary):
 	var character: CharacterData = CharacterData.new()
 	character.name = dict["name"]
 	character.job = dict["job"]
-	character.equipments.left_hand.id = dict["equipments"]["left_hand"]
+	character.equipments.left_hand = dict["equipments"]["left_hand"]
 	_player.characters.append(character)
