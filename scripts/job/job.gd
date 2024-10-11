8915fd7bf341591ed2a1822f300b539aacbe34d7
Archalos0 <tngblondin.prog@gmail.com>
2024-10-11 12:14:06 +0200

    renaming problem + update loading

--- scripts/job/job.gd
+++ scripts/job/job.gd
@@ -5,7 +5,7 @@ const JOBS_FILE: String = "res://ressources/jobs/jobs.json"
 var job_name: String = ""
 var level: int = 1
 
-var actions: Array[Action2] = []
+var actions: Array[Action] = []
 
 var animations: SpriteFrames
 
@@ -15,18 +15,25 @@ var animations: SpriteFrames
 func load(job_id: String, job_level: int):
 	job_name = job_id
 	level = job_level
-
-	load_actions()
-
-func load_actions():
-	var content: Dictionary = FileHandler.get_json_content(JOBS_FILE)
 	
+	_load_data()
+
+func _load_data():
+	var content: Dictionary = FileHandler.get_json_content(JOBS_FILE)
+
 	if content.has("error"):
 		return
 	
-	var job_actions: Array = content[job_name]["actions"]
+	var job_data = content[job_name]
+	
+	animations = SpriteFrames.new()
+	animations = load(job_data["sprite_frames"]) as SpriteFrames
+	
+	_load_actions(job_data["actions"])
+	
+func _load_actions(job_actions: Array):
 	
 	for action_id in job_actions:
-		actions.append(Action2.new())
-		actions[actions.size() - 1] = Action2.load(action_id)
+		actions.append(Action.new())
+		actions[actions.size() - 1] = Action.load(action_id)
 	
