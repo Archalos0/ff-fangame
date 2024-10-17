b2d7173593c7726b436cdcfdfc5a799636dc839c
Archalos0 <tngblondin.prog@gmail.com>
2024-10-17 11:49:26 +0200

    add spells file

--- scripts/utils/files.gd
+++ scripts/utils/files.gd
@@ -2,3 +2,4 @@ class_name Files
 
 const JOBS_FILE = "res://ressources/jobs/jobs.json"
 const EQUIPMENTS_FILE = "res://ressources/equipments/equipments.json"
+const SPELLS_FILE = "res://ressources/spells/spells.json"
