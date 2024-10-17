edfc91ebfd5b4da8a83bb646b86b85274207d304
Archalos0 <tngblondin.prog@gmail.com>
2024-10-17 15:16:44 +0200

    load spell specifically

--- scripts/ability/spells.gd
+++ scripts/ability/spells.gd
@@ -6,13 +6,10 @@ var slot_3: Spell = null
 
 func load(spells_data: SpellsData):
 	if spells_data.first_slot_spell_id != "":
-		slot_1 = Spell.new()
-		slot_1.load(spells_data.first_slot_spell_id)
+		slot_1 = Spell.from_id(spells_data.first_slot_spell_id)
 	
 	if spells_data.second_slot_spell_id != "":
-		slot_2 = Spell.new()
-		slot_2.load(spells_data.second_slot_spell_id)
+		slot_2 = Spell.from_id(spells_data.second_slot_spell_id)
 	
 	if spells_data.third_slot_spell_id != "":
-		slot_3 = Spell.new()
-		slot_3.load(spells_data.third_slot_spell_id)
+		slot_3 = Spell.from_id(spells_data.third_slot_spell_id)
