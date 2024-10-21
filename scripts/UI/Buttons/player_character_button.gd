14d01765e13dedc759595d5114c48bad93f66fe8
Archalos0 <tngblondin.prog@gmail.com>
2024-10-21 14:41:17 +0200

    update player character selection menu

--- scripts/UI/Buttons/player_character_button.gd
+++ scripts/UI/Buttons/player_character_button.gd
@@ -1,5 +1,8 @@
 class_name PlayerCharacterButton extends CommandButton
 
+signal character_selected(character: Battler)
+signal change_mode_selection()
+
 @onready var h_box_container: HBoxContainer = $HBoxContainer
 
 @onready var character_name: Label = $HBoxContainer/CharacterName
@@ -26,3 +29,19 @@ func set_is_selected(p_is_selected: bool):
 		character.arrow_character_selection.visible = true
 	else:
 		character.arrow_character_selection.visible = false
+
+func _on_gui_input(event: InputEvent) -> void:
+	if event is InputEventKey:
+		if event.is_action_pressed("Change_Mode_Selection"):
+			change_mode_selection.emit()
+
+func _on_focus_entered() -> void:
+	set_is_selected(true)
+
+func _on_focus_exited() -> void:
+	set_is_selected(false)
+
+func _on_button_up() -> void:
+	var arr: Array[Battler] = []
+	arr.append(character)
+	character_selected.emit(arr)
--- scripts/UI/Menu/player_selection_menu.gd
+++ scripts/UI/Menu/player_selection_menu.gd
@@ -1,11 +1,81 @@
-class_name PlayerSelectionMenu extends CharacterMenuHandler
+class_name PlayerSelectionMenu extends Panel
 
-const CHARACTER_INFORMATIONS = preload("res://scenes/UI/character_informations.tscn")
+signal characters_selected(characters: Array[Battler])
+
+@onready var character_1: PlayerCharacterButton = $Buttons/Character1
+@onready var character_2: PlayerCharacterButton = $Buttons/Character2
+@onready var character_3: PlayerCharacterButton = $Buttons/Character3
+@onready var character_4: PlayerCharacterButton = $Buttons/Character4
+
+@onready var buttons: VBoxContainer = $Buttons
+
+enum MODE_SELECTION {
+	SINGLE,
+	MULTIPLE,
+}
+
+var _has_focus: bool = false
+var _authorize_multiple_selection: bool = false
+var _current_mode_selection: MODE_SELECTION = MODE_SELECTION.SINGLE 
 
 func load_characters(p_player_characters: Array[Battler]) -> void:
-	for character in p_player_characters:
-		var characterDataUI = CHARACTER_INFORMATIONS.instantiate()
-		_buttons.add_child(characterDataUI)
-		characterDataUI.h_box_container.custom_minimum_size = Vector2(0,50)
-		characterDataUI.set_character(character)
-	super(p_player_characters)
+	character_1.set_character(p_player_characters[0])
+	character_2.set_character(p_player_characters[1])
+	character_3.set_character(p_player_characters[2])
+	character_4.set_character(p_player_characters[3])
+	
+func set_focus_state(p_focus_state: bool, p_authorize_multiple_selection: bool = false):
+	_has_focus = p_focus_state
+	_authorize_multiple_selection = p_authorize_multiple_selection
+	
+	if not _has_focus:
+		character_1.focus_mode = Control.FOCUS_NONE
+		character_1.set_is_selected(false)
+		character_2.focus_mode = Control.FOCUS_NONE
+		character_2.set_is_selected(false)
+		character_3.focus_mode = Control.FOCUS_NONE
+		character_3.set_is_selected(false)
+		character_4.focus_mode = Control.FOCUS_NONE
+		character_4.set_is_selected(false)
+		return
+		
+	character_1.focus_mode = Control.FOCUS_ALL
+	character_2.focus_mode = Control.FOCUS_ALL
+	character_3.focus_mode = Control.FOCUS_ALL
+	character_4.focus_mode = Control.FOCUS_ALL
+	character_1.grab_focus()
+
+func _switch_selection_mode() -> void:
+	if _authorize_multiple_selection == true:
+		match _current_mode_selection:
+			MODE_SELECTION.SINGLE:
+				_current_mode_selection = MODE_SELECTION.MULTIPLE
+				focus_mode = Control.FOCUS_ALL
+				grab_focus()
+				for character_button: PlayerCharacterButton in buttons.get_children():
+					character_button.focus_mode = Control.FOCUS_NONE
+					character_button.set_is_selected(true)
+					
+			MODE_SELECTION.MULTIPLE:
+				_current_mode_selection = MODE_SELECTION.SINGLE
+				focus_mode = Control.FOCUS_NONE
+				for character_button: PlayerCharacterButton in buttons.get_children():
+					character_button.focus_mode = Control.FOCUS_ALL
+					character_button.set_is_selected(false)
+				
+				buttons.get_child(0).grab_focus()
+	else:
+		push_error("impossible de changer le mode de selection")
+
+func _on_gui_input(event: InputEvent) -> void:
+	if event is InputEventKey:
+		if event.is_action_pressed("Validate") and _current_mode_selection == MODE_SELECTION.MULTIPLE:
+			var arr: Array[Battler] = []
+			for character_button: PlayerCharacterButton in buttons.get_children():
+				arr.append(character_button.character)
+			_on_character_selected(arr)
+		if event.is_action_pressed("Change_Mode_Selection"):
+			_switch_selection_mode()
+
+func _on_character_selected(character: Array[Battler]) -> void:
+	characters_selected.emit(character)
