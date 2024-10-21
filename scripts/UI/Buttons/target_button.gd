0748003ca90bbc86a7fad726d9d63e74bb150aa4
Archalos0 <tngblondin.prog@gmail.com>
2024-10-21 11:35:36 +0200

    add arrow selection one the target

--- scripts/UI/Buttons/target_button.gd
+++ scripts/UI/Buttons/target_button.gd
@@ -11,19 +11,13 @@ func set_character(new_character: Battler):
 	character = new_character
 	text = character.get_character_name()
 
-	
-func set_is_selected(p_is_selected: bool):
-	super(p_is_selected)
-	if _is_selected:
-		character.arrow_character_selection.visible = true
-	else:
-		character.arrow_character_selection.visible = false
-
 func _on_focus_entered() -> void:
 	on_focus_entered()
+	character.arrow_character_selection.visible = true
 
 func _on_focus_exited() -> void:
 	on_focus_exited()
+	character.arrow_character_selection.visible = false
 
 func _on_button_up() -> void:
 	target_button_selected.emit(character)
